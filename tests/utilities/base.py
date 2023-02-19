b40f16bd0223d4d09ba3ae62611dd52bff7ead20
Yoodahun <dahun4032@gmail.com>
2023-02-19 12:24:03 +0900

    ADD verify_link_presence

--- tests/utilities/base.py
+++ tests/utilities/base.py
@@ -1,6 +1,18 @@
 from pytest import fixture, mark
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 @mark.usefixtures("setup_and_teardown")
 class DriverFactory:
-    pass
+
+    def verify_link_presence(self, text) -> WebElement:
+        element = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located(
+                (By.LINK_TEXT, text)
+            )
+        )
+
+        return element
