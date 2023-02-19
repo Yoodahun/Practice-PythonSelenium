bb299b0a2f8ece8d1dbfe8e9659de838b8767968
Yoodahun <dahun4032@gmail.com>
2023-02-19 11:46:40 +0900

    ADD checkout

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,14 +1,27 @@
-from typing import ClassVar
 from selenium.webdriver import Chrome
 
+from src.pages.checkout_page import CheckOutPage
 from src.pages.home_page import HomePage
 from utilities.base import DriverFactory
 
 
 class TestOne(DriverFactory):
-    driver: ClassVar[Chrome]
+    driver: Chrome
 
     def test_e2e(self):
         home_page = HomePage(self.driver)
+        checkout_page = CheckOutPage(self.driver)
+
         home_page.shop_item().click()
+        cards = checkout_page.card_title
+
+        for i in range(len(cards)):
+            print(cards[i].text)
+
+            if cards[i].text == "Blackberry":
+                checkout_page.card_footer[i].click()
+                break
+
+        checkout_page.checkout_button.click()
+
 
