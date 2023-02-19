79023fb4ac0ffd25432476144b08f2b637f5a50b
Yoodahun <dahun4032@gmail.com>
2023-02-19 16:17:14 +0900

    ADD homepage data

---
+++ tests/test_data/homepage_data.py
@@ -0,0 +1,7 @@
+
+class HomePageData:
+
+    TEST_HOME_PAGE_DATA = [
+        {"first_name": "test01", "email": "test.com", "gender": "Male"},
+        {"first_name": "test02", "email": "test.com", "gender": "Female"}
+    ]
